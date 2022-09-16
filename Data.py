d49c1800909ba7734b19e45411947453b05274ff
pydevazmi <pydevazmi@gmail.com>
2022-09-16 23:04:28 +0200

    Editing Data-Report

--- Data.py
+++ Data.py
@@ -1,5 +1,5 @@
 coffee = [
-    {"report": {"Water": 400, "Milk": 200, "Coffee": 100, "Money": 0},
+    {"report": {"Water": 400, "Milk": 550, "Coffee": 100, "Money": 0},
      "latte": {"Water": 200, "Milk": 150, "Coffee": 24, "Money": 2.50},
      "espresso": {"Water": 50, "Milk": 0, "Coffee": 24, "Money": 1.50},
      "cappuccino": {"Water": 250, "Milk": 100, "Coffee": 24, "Money": 3.00}}]
